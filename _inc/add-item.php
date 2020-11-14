b0c28ec07554255fe0207c7e341e632e355b24fb
Ucenmarek <marek.ivan777@gmail.com>
2020-11-14 06:12:44 +0100

    premenovanie na add-item

---
+++ _inc/add-item.php
@@ -0,0 +1,25 @@
+<?php
+//skusobny vypis dat
+/*echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+die($_POST['message']);*/
+//include
+require 'config.php';
+
+// vlozenie do dt
+$id = $database->insert('ithems', [
+    'text' =>  $_POST["message"]
+
+]);
+
+// uspešné zapis do dtb a navrat na index
+ if ($id){
+   /*  echo 'uspesny zapis do dtb <hr>';
+    echo '<a href="/tapp/index.php">back home</a>';
+    header('location:http://localhost/tapp/index.php');*/
+    //echo'ok';
+    die('ok');
+ }
+

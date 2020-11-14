5707dd66005217488b7034f0c9c2946e17a2280d
Ucenmarek <marek.ivan777@gmail.com>
2020-11-14 06:12:02 +0100

    pridane edit ithem

---
+++ _inc/edit-item.php
@@ -0,0 +1,28 @@
+<?php
+/*skusobny vypis dat
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+die();*/
+
+/* die($_POST['message']); */
+//include
+require 'config.php';
+
+// update  dt
+$id = $database->update
+    ('ithems', 
+    ['text' =>  $_POST["message"]],
+    ['id' => $_POST ['id']]
+
+  );
+
+// uspešné update do dtb a navrat na index
+ if ($id){
+   
+    header('location:http://localhost/tapp/index.php');
+   
+    
+ }
+

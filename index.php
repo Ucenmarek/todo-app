8f78fa84d23540cc398be6ab1c374c95156111bd
Ucenmarek <marek.ivan777@gmail.com>
2020-11-18 06:44:40 +0100

    zmena delete na ikonu v zozname

--- index.php
+++ index.php
@@ -19,12 +19,15 @@ $data = $database -> select('ithems',['id','text']);
 
         <?php
                 foreach ($data as $item) {
-                    echo '<li class="list-group-item ">';
+                    echo '<li id="ithem-'.$item['id'].'" class="list-group-item ">';
                     echo $item ['text'];
                     //echo '<div class = "row">';
                         echo '<div class ="controls float-right">';
                         echo '<a href = "edit.php?id='.$item['id'].'" class="edit-link">edit</a>';
-                        echo '<a href = "delete.php?id=' . $item['id'] . '" class=" delete-link ">delete</a>';
+                        echo '<a href = "delete.php?id=' . $item['id'] . '" class=" delete-link "><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg></a>';
                         echo '</div>';
                    // echo '</div>';
                     echo '</li>';

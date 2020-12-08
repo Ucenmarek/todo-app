786f9ba365203fa872d5dfb741c9b26392d2f192
Ucenmarek <marek.ivan777@gmail.com>
2020-12-08 05:50:42 +0100

    zmena yz texarea na h3 a p

--- delete.php
+++ delete.php
@@ -5,6 +5,10 @@ $item = $database->get("ithems", "text", [
     "id" => $_GET['id']
 ]);
 
+$autor = $database->get("ithems", "autor", [
+    "id" => $_GET['id']
+]);
+
 // vypis ithem podla'id'
 //echo '<pre>';
 //print_r($item);
@@ -15,7 +19,7 @@ include_once "shared/header.php";
 
 
 <div class="page-header">
-    <h1>VERY MUCH TODO DELETE</h1>
+    <h1>MAZANIE ZÁZNAMOV</h1>
     <hr>
 </div>
 
@@ -23,12 +27,15 @@ include_once "shared/header.php";
 <div class="row">
     <form id="delete-form" action="_inc/delete-item.php" class="col-sm-6" method="post">
         <p class="form-group ">
-            <textarea disabled name="message" id="text" cols="30" rows="1" class="form-control"><?php echo $item ?></textarea>
+            <h3><?php echo $item ?></h3>
+            
+            <p class = "autor"><?php echo $autor ?></p>
+            <!-- <textarea disabled name="message" id="text" cols="30" rows="2" class="form-control"><?php echo $item ?>\p<?php echo $autor ?></textarea> -->
         </p>
         <p class="form-group ">
             <input name="id" type="hidden" value="<?php echo $_GET['id'] ?>">
             <button type="submit" class=" btn-sm btn-danger">Delete ithems</button>
-            <span class="controls"><a href="<?php echo $site_url?>"class="back-link ">back</a></span>
+            <span class="controls"><a href="<?php echo $site_url ?>" class="back-link ">back</a></span>
         </p>
     </form>
 </div>

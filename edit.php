66ac80ae3abf2d4d785936df0399c0af1b409a07
Ucenmarek <marek.ivan777@gmail.com>
2020-12-08 05:52:08 +0100

    zmena

--- edit.php
+++ edit.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once "_inc/config.php";
-$item = $database->get("ithems", "text", [
+$item = $database->get("ithems", "text",  [
+    "id" => $_GET['id']
+]);
+
+$autor = $database->get("ithems",  "autor", [
     "id" => $_GET['id']
 ]);
 
@@ -15,7 +19,7 @@ include_once "shared/header.php";
 
 
 <div class="page-header">
-    <h1>VERY MUCH TODO EDIT</h1>
+    <h1>EDITOVANIE ZÁZNAMOV</h1>
     <hr>
 </div>
 
@@ -23,12 +27,15 @@ include_once "shared/header.php";
 <div class="row">
     <form id="edit-form" action="_inc/edit-item.php" class="col-sm-6" method="post">
         <p class="form-group ">
-            <textarea name="message" id="text" cols="30" rows="3" class="form-control"><?php echo $item ?></textarea>
+            <textarea name="message" id="text" cols="30" rows="1" class="form-control"><?php echo $item ?></textarea>
+        </p>
+        <p class="form-group ">
+            <textarea name="autor" id="autor" cols="30" rows="1" class="form-control"><?php echo $autor ?></textarea>
         </p>
         <p class="form-group ">
             <input name="id" type="hidden" value="<?php echo $_GET['id'] ?>">
             <button type="submit" class=" btn-sm btn-danger">Edit ithems</button>
-            <span class="controls"><a href="<?php echo $site_url?>"class="back-link ">back</a></span>
+            <span class="controls"><a href="<?php echo $site_url ?>" class="back-link ">back</a></span>
         </p>
     </form>
 </div>
